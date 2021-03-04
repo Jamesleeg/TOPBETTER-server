30b9e1908d37f848e7ca2de2dee04f896bd80740
Jameslee Gambardella <>
2021-03-04 12:05:27 -0500

    Post route defined

---
+++ app/models/post.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+ //  i want my post to have a sport
+ //  title / body \
+ //  owner
+const postSchema = new mongoose.Schema({
+  sport: {
+    type: String,
+    required: true
+  },
+  title: {
+    type: String,
+    required: true
+  },
+  body: {
+    type: String,
+    required: true
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    red: 'User',
+    required: true
+  }
+}, {
+    timestamps: true
+})
+
+module.exports = mongoose.model('Event', eventSchema)
